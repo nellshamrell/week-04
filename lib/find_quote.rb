f0cc4fe0af214380000dcc1762e84e012fcf48ac
Nell Shamrell <nshamrell@bluebox.net>
2011-10-29 17:03:06 -0700

    Fixed two of the failing spec files

--- lib/find_quote.rb
+++ lib/find_quote.rb
@@ -7,3 +7,21 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+
+def find_quote(file_name,line_number=rand())
+ 
+  return "Could not find a quote at this time" unless File.exist?(file_name)
+
+  line_count = 0
+  File.open(file_name) {|f| line_count = f.read.count("\n")}
+
+  line_number = Random.rand(line_count + 1) if line_number > line_count
+
+  text = File.open(file_name).readlines
+  quote = text[line_number].chomp
+end
+
+def search_for_quote(file_name,search_criteria)
+  results = []
+  return results unless File.exist?(file_name)
+end
